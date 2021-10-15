b9bd19892a7753765c73078ed5445427292965af
s1270090-Okazaki <49747862+s1270090-Okazaki@users.noreply.github.com>
2021-10-15 15:52:17 +0900

    commit furigana

---
+++ Assets/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    float move = 0.05f;
+    public GameObject[] prefabs = new GameObject[4];
+    int curvege = 0;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x += move;
+        if(pos.x < -2.5f)
+        {
+            move = 0.05f;
+        }
+        if(pos.x > 2.5f)
+        {
+            move = -0.05f;
+        }
+        transform.position = pos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(prefabs[curvege], pos, Quaternion.identity);
+            curvege++;
+            curvege %= 4;
+        }
+    }
+}
